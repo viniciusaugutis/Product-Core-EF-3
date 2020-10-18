ae5d2d17a0f94509e4f4f9e420a718537f5faec0
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-18 12:41:53 -0300

    Criando CRUD no Controller de categoria

--- ApiCore3/Controllers/CategoryController.cs
+++ ApiCore3/Controllers/CategoryController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiCore3.Data;
 using ApiCore3.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCore3.Controllers
 {
@@ -13,35 +15,57 @@ namespace ApiCore3.Controllers
     public class CategoryController : ControllerBase
     {
 
+
         [HttpGet]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get([FromServices] DataContext context)
         {
-            return new List<Category>();
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            
+            return Ok(categories);
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
         {
-            return new Category();
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound(new { message = "Categoria não encontrada" });
+            }
+
+            return Ok(category);
         }
 
 
         [HttpPost]
-        public async Task<ActionResult<Category>> Post([FromBody] Category model)
+        public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            return Ok(model);
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new
+                {
+                    message = "Não foi possivel criar a categoria"
+                }
+                );
+            }
         }
 
         [HttpPut]
         [Route("{id:int}")]
 
-        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model, [FromServices] DataContext context)
         {
             if (model.Id != id)
             {
@@ -53,26 +77,57 @@ namespace ApiCore3.Controllers
                 return BadRequest(ModelState);
             }
 
-            return Ok(model);
+            try
+            {
+                context.Entry<Category>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateConcurrencyException)
+            {
+                return BadRequest(new
+                {
+                    message = "Essa categoria já foi atualizada"
+                });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new
+                {
+                    message = "Não foi possivel atualizar a categoria"
+                });
+            }
         }
 
         [HttpDelete]
         [Route("{id:int}")]
 
-        public async Task<ActionResult<Category>> Delete(int id, [FromBody] Category model)
+        public async Task<ActionResult<Category>> Delete(int id, [FromServices] DataContext context)
         {
-            if (model.Id != id)
+
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
             {
                 return NotFound(new { message = "Categoria não encontrada" });
             }
 
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+
+                return Ok(new { message = "Categoria removida com sucesso" });
+
             }
-
-            return Ok(model);
-
+            catch (Exception)
+            {
+                return BadRequest(new
+                {
+                    message = "Não foi possivel atualizar a categoria"
+                });
+            }
         }
+
     }
+
 }
--- ApiCore3/Startup.cs
+++ ApiCore3/Startup.cs
@@ -1,5 +1,7 @@
+using ApiCore3.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,8 @@ namespace ApiCore3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+            services.AddScoped<DataContext, DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
