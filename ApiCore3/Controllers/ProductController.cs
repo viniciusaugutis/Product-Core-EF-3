b36dce90343103f8bea7814d488565669564a984
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-20 19:08:38 -0300

    Criando controller de produto

---
+++ ApiCore3/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiCore3.Data;
+using ApiCore3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCore3.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
