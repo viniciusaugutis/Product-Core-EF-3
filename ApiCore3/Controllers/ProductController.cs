e164bd79b26b8e825bcc57301e5010df94c1e476
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-10-20 19:18:30 -0300

    Finalizando controller de produto

--- ApiCore3/Controllers/ProductController.cs
+++ ApiCore3/Controllers/ProductController.cs
@@ -21,5 +21,41 @@ namespace ApiCore3.Controllers
 
             return Ok(products);
         }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            var product = await context.Products.Include(x => x.Category).AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+        public async Task<ActionResult<List<Product>>> GetByCategoryId([FromServices] DataContext context, int categoryId)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().Where(x => x.CategoryId == categoryId).ToListAsync();
+
+            return Ok(products);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+
+            } else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
     }
 }
